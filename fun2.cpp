d76df91294efdfd20f854446f5bbd4975cb6eaf3
scue <linkscue@gmail.com>
2014-12-03 01:20:45 +0800

    add 函数重载

--- fun2.cpp
+++ fun2.cpp
@@ -25,6 +25,9 @@ void duplicate_p(int*, int*, int*);
 int divide(int, int=2);
 // int divide(int, int b=2); 与上式相等
 
+/* 函数重载 */
+float divide(float, float=2.0);
+
 int main(int argc, char *argv[])
 {
     int x(1), y(2), z(3);
@@ -35,8 +38,15 @@ int main(int argc, char *argv[])
     duplicate_p(&x,&y,&z);
     std::cout << "x: "<< x << " y: "<< y << " z: " << z << std::endl;
 
-    std::cout << "默认参数值: 12/defaut = " << divide(12) << std::endl;
-    std::cout << "默认参数值: 12/3 = " << divide(12, 3) << std::endl;
+    /* 这里若不定义x1,y1变量，而直接 divide(12,3), 编译器不能知道将调用哪个函数 */
+    /* 报错内容: error: call to 'divide' is ambiguous */
+    int x1(12),y1(3);
+    std::cout << "默认参数值: 12/defaut = " << divide(x1) << std::endl;
+    std::cout << "默认参数值: 12/3 = " << divide(x1,y1) << std::endl;
+
+    float a(15.1), b(3.8);
+    std::cout << "默认参数值: 15.1/defaut = " << divide(a) << std::endl;
+    std::cout << "默认参数值: 15.1/3.8 = " << divide(a,b) << std::endl;
 
     return 0;
 }
@@ -65,5 +75,9 @@ void duplicate_p(int *a, int *b, int *c){
  * 参数默认值
  */
 int divide(int a, int b){
-    return (int)a/b;
+    return a/b;
+}
+
+float divide(float a, float b){
+    return a/b;
 }
